8540c18418814a5a2c945559c96092564d0c2a0d
neelansh27 <neelanshsharma30@gmail.com>
2024-04-16 00:26:55 +0530

    Make input page working

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, send_file
 import pickle
 import geocoder
 from model import label_encoders
+import numpy as np
 
 with open('xgb_model_6mse.pkl', 'rb') as f:
     model = pickle.load(f)
@@ -19,8 +20,9 @@ def input():
     emmision=0
     if request.method=='POST':
         comp = request.form.get('Comp_name')
+        print(comp)
         v_class = request.form.get('v_class')
-        eng_size = int(request.form.get('eng_size'))
+        eng_size = float(request.form.get('eng_size'))
         cyl = int(request.form.get('cyl'))
         trans = request.form.get('trans')
         f_type = request.form.get('f_type')
@@ -28,22 +30,22 @@ def input():
         hwy = int(request.form.get('hwy'))
         comb = int(request.form.get('comb'))
         cons=int(request.form.get('consump'))
-        start = request.form.get('start')
-        end= request.form.get('end')
+        # start = request.form.get('start')
+        # end= request.form.get('end')
 
 
-        comp_en=label_encoders['Make'].transform(comp)
-        v_class_en=label_encoders['Vehicle Class'].transform(v_class)
-        trans_en=label_encoders['Transmission'].transform(trans)
-        f_type_en=label_encoders['Fuel Type'].transform(f_type)
+        comp_en=label_encoders['Make'].transform([comp.upper()])
+        v_class_en=label_encoders['Vehicle Class'].transform([v_class.upper()])
+        trans_en=label_encoders['Transmission'].transform([trans])
+        f_type_en=label_encoders['Fuel Type'].transform([f_type.upper()])
 
-        input=[[comp_en,v_class_en,eng_size,cyl,trans_en,f_type_en,city,hwy,comb,cons]]
-        
+        input=np.array([comp_en[0],v_class_en[0],eng_size,cyl,trans_en[0],f_type_en[0],city,hwy,comb,cons])
+        input=input.reshape(1,10)
         emmision_output=model.predict(input)
-        dist=geocoder.distance(start,end)
+        dist=10
 
         emmision=dist*emmision_output
-
+        return render_template('hello.html', result=emmision[0])
     return render_template('hello.html', result=emmision)
 
 if __name__=='__main__':
