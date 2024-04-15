1269ee1c7a29ec9233e78d87404f40ddef9da6d0
ShivangRustagi04 <shivangrustagi04@gmail.com>
2024-04-15 13:28:47 +0530

    "created py"

---
+++ model.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.cluster import KMeans
