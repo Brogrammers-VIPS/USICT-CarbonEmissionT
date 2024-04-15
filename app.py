6fdb79b23f01494ccdea03c37c0884e171122b5c
Shlok Arora <128911480+ShlokArora2709@users.noreply.github.com>
2024-04-15 08:06:44 +0000

    app var names

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template, request, redirect, send_file
+#from model import label_encoders
 
 app=Flask(__name__)
 
 @app.route('/')
 def main():
     return  render_template('index.html')
+
+
+@app.route('/input', methods=['POST'])
+def input():
+    if request.method=='POST':
+        comp = request.form.get('Comp_name')
+        v_class = request.form.get('v_class')
+        eng_size = request.form.get('eng_size')
+        cyl = request.form.get('cyl')
+        trans = request.form.get('trans')
+        f_type = request.form.get('f_type')
+        city = request.form.get('city')
+        hwy = request.form.get('hwy')
+        comb = request.form.get('comb')
+        start = request.form.get('start')
+        end= request.form.get('end')
