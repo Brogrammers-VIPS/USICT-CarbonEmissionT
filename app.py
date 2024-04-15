afc27b988981e377be3ea2f4b605474fc09ca213
Shlok Arora <128911480+ShlokArora2709@users.noreply.github.com>
2024-04-15 09:09:43 +0000

    finished the flask page

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, redirect, send_file
 import pickle
-#from model import label_encoders
+import geocoder
+from model import label_encoders
 
-with open('model.pkl', 'rb') as f:
+with open('xgb_model_6mse.pkl', 'rb') as f:
     model = pickle.load(f)
 
 
@@ -13,18 +14,18 @@ def main():
     return  render_template('index.html')
 
 
-@app.route('/input', methods=['POST'])
+@app.route('/input', methods=['GET', 'POST'])
 def input():
     if request.method=='POST':
         comp = request.form.get('Comp_name')
         v_class = request.form.get('v_class')
-        eng_size = request.form.get('eng_size')
-        cyl = request.form.get('cyl')
+        eng_size = int(request.form.get('eng_size'))
+        cyl = int(request.form.get('cyl'))
         trans = request.form.get('trans')
         f_type = request.form.get('f_type')
-        city = request.form.get('city')
-        hwy = request.form.get('hwy')
-        comb = request.form.get('comb')
+        city = int(request.form.get('city'))
+        hwy = int(request.form.get('hwy'))
+        comb = int(request.form.get('comb'))
         start = request.form.get('start')
         end= request.form.get('end')
 
@@ -35,6 +36,13 @@ def input():
         f_type_en=label_encoders['Fuel Type'].transform(f_type)
 
         input=[[comp_en,v_class_en,eng_size,cyl,trans_en,f_type_en,city,hwy,comb]]
-        emmison_output=model.predict(input)
-
         
+        emmision_output=model.predict(input)
+        dist=geocoder.distance(start,end)
+
+        emmision=dist*emmision_output
+
+    return render_template('imput_output.html', result=emmision)
+
+
+
